fa6b759c0499c5a7e811a432f72a31d6dff5f737
Popa Iulian-Alexandru <iulianp14@yahoo.com>
2024-12-08 12:03:43 +0200

    add photos for art contexts

--- src/components/Contexts.jsx
+++ src/components/Contexts.jsx
@@ -4,7 +4,8 @@ import './Contexts.css';
 
 const Contexts = () => {
 	const { scrollYProgress } = useScroll();
-	const rotate = useTransform(scrollYProgress, [0.5, 0.8], ['0deg', '10deg']);
+	const rotate = useTransform(scrollYProgress, [0.58, 0.9], ['0deg', '10deg']);
+	const rotateInv = useTransform(scrollYProgress, [0.77, 0.85], ['0deg', '-10deg']);
 
 	return (
 		<section className="section">
@@ -24,47 +25,87 @@ const Contexts = () => {
 				</motion.h1>
 			</motion.div>
 			<motion.div
-				className="definition-container"
+				className="definition-container image-container"
 				style={{ rotate }}
 				initial={{ opacity: 0, translateY: 50 }}
 				whileInView={{ opacity: 1, translateY: 0 }}
 				transition={{ duration: 1 }}
-				viewport={{ amount: 0.5 }}
+				viewport={{ amount: 0.1 }}
 			>
-				<div className="definition">
-					<strong>Museum Context: </strong>
-					<span>
-						Art is an interactive experience, shaped by the design of exhibitions, the viewer&apos;s
-						engagement, and the social setting, which influence its appreciation and emotional impact.
-					</span>
+				<div>
+					<img
+						src="https://d26jxt5097u8sr.cloudfront.net/s3fs-public/2021-12/2021-11-18_Hamilton_WtC_Visitors_27.jpg"
+						alt="Art in Museum context"
+						width={300}
+						height={400}
+					/>
+					<div className="definition">
+						<strong>Museum Context: </strong>
+						<span>
+							Art is an interactive experience, shaped by the design of exhibitions, the viewer&apos;s
+							engagement, and the social setting, which influence its appreciation and emotional impact.
+						</span>
+					</div>
 				</div>
-				<div className="definition">
-					<strong>Natural Environments: </strong>
-					<span>
-						Art often aligns with the innate human tendency to find beauty in nature, evoking peace,
-						connection, and positive emotional responses.
-					</span>
+				<div>
+					<img
+						src="https://images.saatchiart.com/saatchi/2064431/art/10382271/9444887-ZMYEMCUU-7.jpg"
+						alt="Art in Natural Environments context"
+						width={300}
+						height={400}
+					/>
+					<div className="definition">
+						<strong>Natural Environments: </strong>
+						<span>
+							Art often aligns with the innate human tendency to find beauty in nature, evoking peace,
+							connection, and positive emotional responses.
+						</span>
+					</div>
 				</div>
-				<div className="definition">
-					<strong>Urban/Built Environments: </strong>
-					<span>
-						Art in cities and architecture enhances aesthetic appreciation while influencing social
-						behaviors and relationships through designed spaces.
-					</span>
+				<div>
+					<img
+						src="https://ooiio.com/wp-content/uploads/2024/01/OOIIO-Arquitectura-Plaza-San-Miguel-M-1080x810.webp"
+						alt="Art in Urban/Built Environments context"
+						width={300}
+						height={400}
+					/>
+					<div className="definition">
+						<strong>Urban/Built Environments: </strong>
+						<span>
+							Art in cities and architecture enhances aesthetic appreciation while influencing social
+							behaviors and relationships through designed spaces.
+						</span>
+					</div>
 				</div>
-				<div className="definition">
-					<strong>Ecological Contexts: </strong>
-					<span>
-						Art in everyday or ecological settings becomes a multidimensional experience, linked to
-						emotional resonance, intellectual engagement, and a sense of discovery.
-					</span>
+				<div>
+					<img
+						src="https://www.switchonpaper.com/wp-content/uploads/2020/09/A_day_in_hell_150x165cm_2016_.jpg"
+						alt="Art in Ecological context"
+						width={300}
+						height={400}
+					/>
+					<div className="definition">
+						<strong>Ecological Contexts: </strong>
+						<span>
+							Art in everyday or ecological settings becomes a multidimensional experience, linked to
+							emotional resonance, intellectual engagement, and a sense of discovery.
+						</span>
+					</div>
 				</div>
-				<div className="definition">
-					<strong>Laboratory Settings: </strong>
-					<span>
-						Art is studied as a cognitive and emotional process, often losing the richness of context but
-						allowing for controlled insights into perception and reaction.
-					</span>
+				<div>
+					<img
+						src="https://bfafinearts.sva.edu/wp-content/uploads/BioArt-Lab-Facility-2018_21-08-18_009_x1800.jpg"
+						alt="Art in Laboratory Settings context"
+						width={300}
+						height={400}
+					/>
+					<div className="definition">
+						<strong>Laboratory Settings: </strong>
+						<span>
+							Art is studied as a cognitive and emotional process, often losing the richness of context
+							but allowing for controlled insights into perception and reaction.
+						</span>
+					</div>
 				</div>
 			</motion.div>
 			<motion.div
@@ -89,6 +130,7 @@ const Contexts = () => {
 			</motion.h2>
 			<motion.div
 				className="definition-container"
+				style={{ rotate: rotateInv }}
 				initial={{ opacity: 0, translateY: 50 }}
 				whileInView={{ opacity: 1, translateY: 0 }}
 				transition={{ duration: 1 }}
